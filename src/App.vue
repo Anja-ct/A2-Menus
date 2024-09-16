470f13fbf72135fa93fa9948d214f648fe1a117c
Anja-ct <159250418+Anja-ct@users.noreply.github.com>
2024-09-16 18:09:09 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const msgVariable = ref('hello world');
+
+const handleClick = (event) => {
+  msgVariable.value = 'hejjj';
+};
+
+onMounted(() => {
+  window.addEventListener('click', handleClick);
+});
+</script>
+
+<template>
+  <h1>{{ msgVariable }}</h1>
+  <div class="square"></div>
+</template>
+
+<style scoped>
+/* Styling af kvadrat */
+.square {
+  width: 300px;
+  height: 100px;
+  background-color: red;
+  margin-top: 20px;
+}
+</style>
